12f413c716c2ca562590088ea70efa54fcd87e96
akssingh0102 <akashsingh095@gmail.com>
2024-04-02 22:44:16 +0530

    react usestate

--- week-04/react-foundation-with-react/react-first-app/src/App.jsx
+++ week-04/react-foundation-with-react/react-first-app/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+const getRandomNumber = () => {
+  return Math.round(Math.random() * 10);
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [rand, setRand] = useState(getRandomNumber());
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">
+        <button onClick={() => setRand(() => getRandomNumber())}>
+          Random number is {rand}
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
