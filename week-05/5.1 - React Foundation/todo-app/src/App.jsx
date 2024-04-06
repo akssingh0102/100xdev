82123abcbde0aefdb1df735792325574ba01782d
akssingh0102 <akashsingh095@gmail.com>
2024-04-06 18:33:47 +0530

    dummy todo react

--- week-05/5.1 - React Foundation/todo-app/src/App.jsx
+++ week-05/5.1 - React Foundation/todo-app/src/App.jsx
@@ -1,25 +1,53 @@
 import "./App.css";
 import { useState } from "react";
 
+const initialTodoState = [
+  {
+    title: "Go to gym",
+    description: "Go to gym from 7-9",
+    completed: false,
+  },
+  {
+    title: "Study DSA",
+    description: "Study DSA from 9-11",
+    completed: true,
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(initialTodoState);
+
+  const addTodo = () => {
+    setTodos([
+      ...todos,
+      {
+        title: "Eat Food",
+        description: "Eat food 3 times a day",
+        completed: true,
+      },
+    ]);
+  };
   return (
     <div>
-      <CustomButton />
-      <CustomButton />
-      <CustomButton />
-      <CustomButton />
+      <button onClick={addTodo}>Add Todo</button>
+      {todos.map((todo, index) => (
+        <Todo
+          title={todo.title}
+          description={todo.description}
+          key={index}
+        ></Todo>
+      ))}
     </div>
   );
 }
 
-function CustomButton() {
-  const [count, setCount] = useState(0);
-
-  const onClickHandler = () => {
-    setCount(count + 1);
-  };
-
-  return <button onClick={onClickHandler}>{count}</button>;
+function Todo(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.description}</h2>
+    </div>
+  );
 }
 
 export default App;
