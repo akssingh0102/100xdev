93998dd72da163d4850df5d855ce9a0b33482b41
akssingh0102 <akashsingh095@gmail.com>
2024-04-10 09:16:28 +0530

    .

--- week-06/experiments/experiments-with-react/src/App.jsx
+++ week-06/experiments/experiments-with-react/src/App.jsx
@@ -1,20 +1,40 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <Headers title="Akash"></Headers>
-      <Headers title="Singh"></Headers>
+      <HeadersWithButton/>
+      <Text inputName={"Akash"}></Text>
+      <br />
     </div>
-  )
+  );
 }
 
-const Headers = ({title}) => {
-  return <div>
-    {title}
-  </div>
-}
 
-export default App
+
+const HeadersWithButton = () => {
+  const [name, setName] = useState("Akash");
+
+  const changeName = () => {
+    return setName(String(Math.random()));
+  };
+
+  return (
+    <>
+      <button type="button" onClick={changeName}>
+        Click me
+      </button>
+      <br />
+      <Text inputName={name}></Text>
+      <br />
+    </>
+  );
+};
+
+const Text = ({ inputName }) => {
+  return <div>My name is {inputName}</div>;
+};
+
+export default App;
