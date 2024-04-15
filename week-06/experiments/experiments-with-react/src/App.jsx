88f3f4024e40a050b1a0c8dc9687f45d7b392563
akssingh0102 <akashsingh095@gmail.com>
2024-04-15 19:04:56 +0530

    .

--- week-06/experiments/experiments-with-react/src/App.jsx
+++ week-06/experiments/experiments-with-react/src/App.jsx
@@ -1,59 +1,21 @@
 import { useState } from "react";
 
-const initialTodos = [
-  {
-    id: 1,
-    title: "go to gym",
-    description: "go to gym today",
-  },
-  {
-    id: 2,
-    title: "go to office",
-    description: "go to office today",
-  },
-  {
-    id: 3,
-    title: "go to club",
-    description: "go to club today",
-  },
-];
-
-let idCount = 4;
-
 function App() {
-  const [todos, setTodos] = useState(initialTodos);
-
-  const addTodo = () => {
-    setTodos([
-      ...todos,
-      {
-        id: idCount,
-        title: "go to park",
-        description: "go to park today",
-      },
-    ]);
-
-    console.log(`Counter value is ${idCount}`);
-    idCount = idCount + 1;
-  };
-
   return (
     <>
-    <button type="button" onClick={addTodo}>Add Todo</button>
-      {todos.map((todo) => (
-        <Todo key={todo.id} {...todo}></Todo>
-      ))}
+      <CardWrapper>
+        <TextComponent/>
+      </CardWrapper>
     </>
   );
 }
 
-const Todo = ({ title, description, id }) => {
-  return (
-    <div>
-      <h1>{title}</h1>
-      <h5>{description}</h5>
-    </div>
-  );
+const TextComponent = () => {
+  return <>Hi There</>;
+};
+
+const CardWrapper = ({ children }) => {
+  return <div style={{ border: "2px solid black" }}>{children}</div>;
 };
 
 export default App;
