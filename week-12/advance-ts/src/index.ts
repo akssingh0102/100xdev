c46fe0229f34385f1c9a970320896b5ece3b28bb
akssingh0102 <akashsingh095@gmail.com>
2024-06-01 20:27:16 +0530

    tsc

--- week-12/advance-ts/src/index.ts
+++ week-12/advance-ts/src/index.ts
@@ -1,18 +1,53 @@
-interface User {
-    name: string;
-    age: number;
-}
+// interface User {
+//     name: string;
+//     age: number;
+// }
 
-function sumOfAge(user1: User, user2: User) {
-    return user1.age + user2.age;
+// function sumOfAge(user1: User, user2: User) {
+//     return user1.age + user2.age;
+// };
+
+// // Example usage
+// const result = sumOfAge({
+//     name: "akash",
+//     age: 20
+// }, {
+//     name: "singh",
+//     age: 21
+// });
+// console.log(result);
+
+
+// PICK
+// interface User {
+//     id: number;
+//     name: string;
+//     email: string;
+//     createdAt: Date;
+// }
+
+// // For a profile display, only pick `name` and `email`
+// type UserProfile = Pick<User, 'name' | 'email'>;
+
+// const displayUserProfile = (user: UserProfile) => {
+//     console.log(`Name: ${user.name}, Email: ${user.email}`);
+// };
+
+
+// PARTIAL
+interface User {
+    id: string;
+    name: string;
+    age: string;
+    email: string;
+    password: string;
 };
 
-// Example usage
-const result = sumOfAge({
-    name: "akash",
-    age: 20
-}, {
-    name: "singh",
-    age: 21
-});
-console.log(result);
+type UpdateProps = Pick<User, 'name' | 'age' | 'email'>
+
+type UpdatePropsOptional = Partial<UpdateProps>
+
+function updateUser(updatedProps: UpdatePropsOptional) {
+    // hit the database tp update the user
+}
+updateUser({})
