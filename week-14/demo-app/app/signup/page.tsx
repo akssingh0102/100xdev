b350b280e4accca0f4d44d4d6cc21fed2252d575
akssingh0102 <akashsingh095@gmail.com>
2024-07-04 08:44:06 +0530

    backend for next

---
+++ week-14/demo-app/app/signup/page.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/display-name */
+/* eslint-disable import/no-anonymous-default-export */
+import { Signup } from '@/app/components/Signup';
+
+export default function () {
+  return <Signup />;
+}
